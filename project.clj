44fb12ab7d688a7914c994c819c90cbd9bb96ca9
Seth Buntin <seth.buntin@gmail.com>
2010-10-26 15:41:55 -0500

    add clojure-contrib as dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject sfteepee "0.3.0"
+(defproject org.buntin/sfteepee "0.3.0"
   :description "A clojure wrapper for Jsch SFTP"
   :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [com.jcraft/jsch "0.1.42"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [lein-clojars "0.6.0"]])
