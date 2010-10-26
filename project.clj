d56db8ce5bb733dd235dc5eea4da0548d0cb796b
Seth Buntin <seth.buntin@gmail.com>
2010-10-26 16:37:33 -0500

    change group id for possible merge into ccollins/sfteepee

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.buntin/sfteepee "0.3.0"
+(defproject sfteepee "0.3.0"
   :description "A clojure wrapper for Jsch SFTP"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
